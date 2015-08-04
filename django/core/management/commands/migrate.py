c65afe32db4e615daba16c324f0235dc3f43e9a4
Peter Schmidt <peter@peterjs.com>
2015-08-04 12:41:50 +1000

    Save rebuilding the loader if we already have it. Best of 3 timings improves from: has_unmigrated_models - Took 17.756 seconds => has_unmigrated_models - Took 17.125 seconds

--- django/core/management/commands/migrate.py
+++ django/core/management/commands/migrate.py
@@ -178,7 +178,7 @@ class Command(BaseCommand):
             if self.verbosity >= 1:
                 self.stdout.write("  No migrations to apply.")
                 # If there's changes that aren't in migrations yet, tell them how to fix it.
-                if has_unmigrated_models():
+                if has_unmigrated_models(loader=executor.loader):
                     self.stdout.write(self.style.NOTICE(
                         "  Your models have changes that are not yet reflected "
                         "in a migration, and so won't be applied."
