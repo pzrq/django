57885730e1decda9f036e6f82e8c0d8a4ea9f193
Peter Schmidt <peter@peterjs.com>
2015-08-04 12:27:07 +1000

    Add timing code so we can get an idea of how expensive this function is.

--- django/db/migrations/utils.py
+++ django/db/migrations/utils.py
@@ -4,6 +4,29 @@ from django.db.migrations.loader import MigrationLoader
 from django.db.migrations.state import ProjectState
 
 
+import time
+
+
+def timethis(func):
+    """
+    Very basic version since we can't easily use Python's timeit.
+    Feel free to improve :)
+    """
+    def wrap(*args, **kwargs):
+        start = time.time()
+        try:
+            result = func(*args, **kwargs)
+            return result
+        finally:
+            msg = '%s - Took %.3f seconds' % (
+                func.__name__, time.time() - start
+            )
+            print(msg)
+
+    return wrap
+
+
+@timethis
 def has_unmigrated_models(loader=None, connection=None):
     """
     Returns True if the currently registered models have changes that are not
